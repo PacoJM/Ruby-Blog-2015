6e964f34bfc6aaea33c465b4ec227049477ddfe2
Paco <paco_j_m_14@hotmail.com>
2015-06-10 18:35:11 +0200

    Añadido edición de datos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,8 +23,24 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
+	def update
+		  @article = Article.find(params[:id])
+ 
+  		if @article.update(article_params)
+    		redirect_to @article
+  		else
+    		render 'edit'
+  		end
+	end
+
 	private
 		def article_params
 			params.require(:article).permit(:title, :text)
 		end
+
+	
 end
